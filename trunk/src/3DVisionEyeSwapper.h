0407f48308d6e78045af0f4ca71a2cc23af54fc3
ChiutaAdrian@gmail.com <ChiutaAdrian@gmail.com@fbadd1e8-f716-280a-d879-3991e87472cd>
2013-12-06 22:49:05 +0000

    Added first revision.

---
+++ trunk/src/3DVisionEyeSwapper.h
@@ -0,0 +1,36 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+///  File:        3DVisionEyeSwapper.h
+///  Description: Includes the headers needed by the application.
+///  Author:      Chiuta Adrian Marius
+///  Created:     23-11-2013
+///
+///  Licensed under the Apache License, Version 2.0 (the "License");
+///  you may not use this file except in compliance with the License.
+///  You may obtain a copy of the License at
+///  http://www.apache.org/licenses/LICENSE-2.0
+///  Unless required by applicable law or agreed to in writing, software
+///  distributed under the License is distributed on an "AS IS" BASIS,
+///  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+///  See the License for the specific language governing permissions and
+///  limitations under the License.
+///
+////////////////////////////////////////////////////////////////////////////////////////////////////
+#ifndef INCLUDED_3DVISIONEYESWAPPER_H
+#define INCLUDED_3DVISIONEYESWAPPER_H
+
+#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+#include <shellapi.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
+
+#include "./resource.h"
+#include "./Registry.h"
+
+#endif // INCLUDED_3DVISIONEYESWAPPER_H
